75e649e39deda1bbe5201502833c42272d736300
sairaj <sairaj2119@gmail.com>
2021-03-04 09:10:00 +0530

    reset

--- src/index.js
+++ src/index.js
@@ -1,20 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { ChakraProvider } from '@chakra-ui/react';
-
-import { initialState, userReducer, UserProvider } from './context/userContext';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <UserProvider initialState={initialState} reducer={userReducer}>
-      <ChakraProvider>
-        <Router>
-          <App />
-        </Router>
-      </ChakraProvider>
-    </UserProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
